6c9d3ad6d6a33923423b761daafa82c5573b8c27
LoneMaaherra <lonemaaherra@gmail.com>
2020-01-24 15:30:32 +0100

    Add images.

--- js/main.js
+++ js/main.js
@@ -2,29 +2,75 @@ let level = null;
 let camera;
 let playerPos;
 let playerVel;
+let playerJumped;
+let tilesInScreenWidth;
+let tilesInScreenHeight;
+
+let ctx;
+let tileSetImg;
 
 
 function setup() {
-    createCanvas(512, 480)
+    createCanvas(512, 480);
+    frameRate(30);
     level = getLevel(0);
+
+    tilesInScreenWidth = ceil(width / TILE_SIZE);
+    tilesInScreenHeight = ceil(height / TILE_SIZE);
+    if (tilesInScreenWidth > level.width) tilesInScreenWidth = level.width;
+    if (tilesInScreenHeight > level.height) tilesInScreenHeight = level.height;
 
     camera = new Vector(0 ,0);
     playerPos = new Vector(TILE_SIZE * 5, TILE_SIZE * 11);
     playerVel = new Vector(0 ,0);
+    playerJumped = false;
+
+
+    ctx = document.getElementById('defaultCanvas0').getContext('2d');
+    tileSetImg = new Image();
+    tileSetImg.src = TILE_SET_PATH;  //= loadImage(TILE_SET_PATH);
+    //tileSetImg = tileSetImg.resize(tileSetImg.width * (TILE_SIZE / SPRITE_TILE_SIZE), tileSetImg.height * (TILE_SIZE / SPRITE_TILE_SIZE));
+
+    /*let str = '';
+    for (let y = 0; y < tileSetImg.height; y+=16) {
+        for (let x = 0; x < tileSetImg.width; x+=16) {
+            let index = (x / 16) + ((y / 16) * (tileSetImg.width / 16)) + 1;
+            str += `${index}:\t[${x},\t${y}],\n`;
+        }
+    }
+    
+    console.log(str);*/
+}
+
+function keyReleased() {
+    if (keyCode === UP_ARROW) playerJumped = true;
 }
 
 function update() {
-    if (keyIsDown(LEFT_ARROW))  playerVel.x -= 0.3;
-    if (keyIsDown(RIGHT_ARROW)) playerVel.x += 0.3; 
-    if (keyIsDown(UP_ARROW) && playerVel.y == 0)    playerVel.y -= 16.7;
-    if (keyIsDown(DOWN_ARROW))  playerVel.y += 0.3;
+    let speed = TILE_SIZE * 0.025;
 
-    playerVel.y += 0.8;
+    if (keyIsDown(LEFT_ARROW))  playerVel.x += -speed;
+    if (keyIsDown(RIGHT_ARROW)) playerVel.x += speed; 
+    if (keyIsDown(UP_ARROW) && !playerJumped && playerVel.y <= 0) { 
+        if (playerVel.y == 0) {
+            playerVel.y -= TILE_SIZE / 4;
+        }
+        else {
+            playerVel.y -= TILE_SIZE / 12;
+        }
+
+        if (playerVel.y < -TILE_SIZE * 0.44) playerJumped = true;
+    };
+
+    let drag = TILE_SIZE * 0.025;
+    if (playerVel.x > -drag && playerVel.x < drag) playerVel.x = 0;
+
+    playerVel.x += -playerVel.x * 0.11;
+    playerVel.y += TILE_SIZE / 31;
+
     playerPos = getNewPosition(playerPos, playerVel);
 
-    playerVel.x += -playerVel.x * 0.04
-
-    moveCamera(playerPos.copy().sub(new Vector(TILE_SIZE * 8, TILE_SIZE * 8)));
+    moveCamera(playerPos.copy().sub(new Vector(TILE_SIZE * floor(tilesInScreenWidth / 2), TILE_SIZE * floor(tilesInScreenHeight / 2))));
 }
 
 function draw() {
@@ -33,7 +79,8 @@ function draw() {
     update();
 
     noStroke();
-    drawLevel();
+    drawLayer(level.background, 0.9);
+    drawLayer(level.foreground, 1);
 
     // player
     fill('rgb(255, 0, 0)');
@@ -43,22 +90,8 @@ function draw() {
 
 function getNewPosition(position, velocity) {
     let newPosition = position.copy().add(velocity);
-
-    // let collisions = getCollisions(position, velocity);
-
-    // let pushDown = function() {
-    //     newPosition.y = floor(newPosition.y / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
-    // }
-    // let pushUp = function() {
-    //     newPosition.y = floor(newPosition.y / TILE_SIZE) * TILE_SIZE;
-    // }
-    // let pushRight = function() {
-    //     newPosition.x = floor(newPosition.x / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
-    // }
-    // let pushLeft = function() {
-    //     newPosition.x = floor(newPosition.x / TILE_SIZE) * TILE_SIZE;
-    // }
     
+    // X
     if (velocity.x <= 0) {
         if (getTile(new Vector(newPosition.x, position.y)) != 0 || getTile(new Vector(newPosition.x, position.y + TILE_SIZE * 0.9))) {
             newPosition.x = floor(newPosition.x / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
@@ -72,6 +105,7 @@ function getNewPosition(position, velocity) {
         }
     }
 
+    // Y
     if (velocity.y <= 0) {
         if (getTile(new Vector(newPosition.x + 0, newPosition.y + 0)) != 0 || getTile(new Vector(newPosition.x + TILE_SIZE * 0.99, newPosition.y + 0))) {
             newPosition.y = floor(newPosition.y / TILE_SIZE) * TILE_SIZE + TILE_SIZE;
@@ -81,88 +115,12 @@ function getNewPosition(position, velocity) {
     else {
         if (getTile(new Vector(newPosition.x + TILE_SIZE * 0.1, newPosition.y + TILE_SIZE)) != 0 || getTile(new Vector(newPosition.x + TILE_SIZE*0.9, newPosition.y + TILE_SIZE))) {
             newPosition.y = floor(newPosition.y / TILE_SIZE) * TILE_SIZE;
+            playerJumped = false;
             velocity.y = 0;
         }
     }
 
-    // if      (collisions[0] && collisions[1] ) { pushDown() }
-    // else if (collisions[2] && collisions[3]) { pushRight() }
-    // else if (collisions[4] && collisions[5]) { pushLeft() }
-    // else if (collisions[6] && collisions[7]) { pushUp() }
-    // else if ((collisions[0] || collisions[1]) && !(collisions[2] || collisions[4])) { pushDown() }
-    // else if ((collisions[6] || collisions[7]) && !(collisions[3] || collisions[5])) { pushUp() }
-    // else if ((collisions[2] || collisions[3]) && !(collisions[0] || collisions[6]) ) { pushRight() }
-    // else if ((collisions[4] || collisions[5]) && !(collisions[2] || collisions[7]) ) { pushLeft() }
-
     return newPosition;
-}
-
-function temp() {
-    
-    let amt1 = 0.0625;
-    let amt2 = 0.03125;
-    let newPos = position.copy().add(velocity);
-
-    // top-left / top-right
-    let tl = newPos.copy().add(new Vector(0, 0)).mult(1 + amt1, 1 + amt2);
-    let tr = newPos.copy().add(new Vector(TILE_SIZE, 0)).mult(1 - amt1, 1 + amt2);
-    
-    // left-top / left-bottom
-    let lt = newPos.copy().add(new Vector(0, 0)).mult(1 + amt2, 1 + amt1);
-    let lb = newPos.copy().add(new Vector(0, TILE_SIZE)).mult(1 + amt2, 1 - amt1);
-
-    // right-top / right-bottom
-    let rt = newPos.copy().add(new Vector(TILE_SIZE, 0)).mult(1 - amt2, 1 + amt1);
-    let rb = newPos.copy().add(new Vector(TILE_SIZE, TILE_SIZE)).mult(1 - amt2, 1 - amt1);
-
-    // bottom-left / bottom-right
-    let bl = newPos.copy().add(new Vector(0, TILE_SIZE)).mult(1 + amt1, 1 - amt2);
-    let br = newPos.copy().add(new Vector(TILE_SIZE, TILE_SIZE)).mult(1 - amt1, 1 - amt2);
-}
-
-function getCollisions(position, velocity) {
-    
-    let amt1 = 4;
-    let amt2 = 1;
-    let newPos = position.copy().add(velocity);
-
-    // top-left / top-right
-    let tl = newPos.copy().add(new Vector(0, 0)).add(new Vector(amt1, amt2));
-    let tr = newPos.copy().add(new Vector(TILE_SIZE, 0)).add(new Vector(-amt1, amt2));
-    
-    // left-top / left-bottom
-    let lt = newPos.copy().add(new Vector(0, 0)).add(new Vector(amt2, amt1));
-    let lb = newPos.copy().add(new Vector(0, TILE_SIZE)).add(new Vector(amt2, -amt1));
-
-    // right-top / right-bottom
-    let rt = newPos.copy().add(new Vector(TILE_SIZE, 0)).add(new Vector(-amt2, amt1));
-    let rb = newPos.copy().add(new Vector(TILE_SIZE, TILE_SIZE)).add(new Vector(-amt2, -amt1));
-
-    // bottom-left / bottom-right
-    let bl = newPos.copy().add(new Vector(0, TILE_SIZE)).add(new Vector(amt1, -amt2));
-    let br = newPos.copy().add(new Vector(TILE_SIZE, TILE_SIZE)).add(new Vector(-amt1, -amt2));
-
-    
-    let collisions = []
-    // player
-    fill('rgb(255, 0, 0)');
-    rect(playerPos.x - camera.x, playerPos.y - camera.y, 
-         TILE_SIZE, TILE_SIZE);
-    stroke(0, 255, 0);
-    strokeWeight(2);
-    for (let point_ of [tl, tr, lt, lb, rt, rb, bl, br]) {
-        point(point_.x - camera.x, point_.y - camera.y);
-
-        point_ = getTileAtPosition(point_);
-
-        if (level.data[point_.y][point_.x] != 0) {
-            collisions.push(true);
-        } else {
-            collisions.push(false);
-        }
-    }
-
-    return collisions;
 }
 
 function getTileAtPosition(position) {
@@ -172,7 +130,7 @@ function getTileAtPosition(position) {
 function getTile(position) {
     let newPos = getTileAtPosition(position);
 
-    return level.data[newPos.y][newPos.x];
+    return level.foreground[newPos.y][newPos.x];
 }
 
 function moveCamera(newPosition) {
@@ -181,21 +139,29 @@ function moveCamera(newPosition) {
     if (camera.x < 0) camera.x = 0;
     else if (camera.x > (level.width * TILE_SIZE - width)) camera.x = (level.width * TILE_SIZE - width);
     if (camera.y < 0) camera.y = 0;
-    else if (camera.y > (level.height * TILE_SIZE - height)) camera.y = (level.height * TILE_SIZE - height);
+    else if (camera.y > (level.height * TILE_SIZE - tilesInScreenHeight * TILE_SIZE)) camera.y = (level.height * TILE_SIZE) - tilesInScreenHeight * TILE_SIZE;
 }
 
-function drawLevel() {
-    let startY = floor(camera.y / TILE_SIZE);
-    let startX = floor(camera.x / TILE_SIZE);
-    for (let y = startY; y < floor(startY + height/TILE_SIZE); y++) {//level.height; y++) {
-       for (let x = startX; x < floor(startX + width/TILE_SIZE) + 1; x++) {
-/*    for (let y = 0; y < level.height; y++) {//level.height; y++) {
-        for (let x = 0; x < level.width; x++) {*/
-            let tile = level.data[y][x];
+function drawLayer(layer, speed) {
+    let startY = floor((camera.y * speed) / TILE_SIZE);
+    let startX = floor((camera.x * speed) / TILE_SIZE);
+    let endY   = floor(startY + tilesInScreenHeight) + 1;
+    let endX   = floor(startX + tilesInScreenWidth) + 1;
+
+    endY = endY <= level.height ? endY : level.height;
+    endX = endX <= level.width ? endX : level.width;
+
+    for (let y = startY; y < endY; y++) {
+       for (let x = startX; x < endX; x++) {
+            let tile = layer[y][x];
             if (!(tile in TILES)) { continue; }
 
-            fill(TILES[tile]);
-            rect(x * TILE_SIZE - camera.x, y * TILE_SIZE - camera.y, TILE_SIZE, TILE_SIZE);
+            let newX = x * TILE_SIZE - camera.x * speed;
+            let newY = y * TILE_SIZE - camera.y * speed;
+            ctx.drawImage(tileSetImg, TILES[tile][0], TILES[tile][1], SPRITE_TILE_SIZE, SPRITE_TILE_SIZE, 
+                                      newX, newY, TILE_SIZE, TILE_SIZE);
+            //fill(TILES[tile]);
+            //rect(x * TILE_SIZE - camera.x * speed, y * TILE_SIZE - camera.y * speed, TILE_SIZE, TILE_SIZE);
         }
     }
 }
@@ -204,7 +170,8 @@ function drawLevel() {
 function getLevel(levelNumber) {
     let newLevel = {...LEVELS[levelNumber]};
 
-    newLevel.data = arrayToMatrix(newLevel.data, newLevel.width);
+    newLevel.foreground = arrayToMatrix(newLevel.foreground, newLevel.width);
+    newLevel.background = arrayToMatrix(newLevel.background, newLevel.width);
     newLevel.activeObjects = arrayToMatrix(newLevel.activeObjects, newLevel.width);
 
     return newLevel;
